a81dfc811d29c1693671f220c83d623b51d9bb4d
Michael du Breuil <wicked.shell.scripts@gmail.com>
2017-03-10 00:28:06 -0700

    Update core.async version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-taskpool "0.1.0"
+(defproject clj-taskpool "0.1.1-SNAPSHOT"
   :description "Simple parallel task pool."
   :url "https://github.com/wickedshell/clj-taskpool"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.395"]])
+                 [org.clojure/core.async "0.3.441"]])
